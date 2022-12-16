88194337c5452327aa1db130008e714e4e74269f
Nahid570 <farajinahid2@gmail.com>
2022-12-16 12:57:16 +0600

    client basic routing setup

---
+++ server/controllers/notesController.js
@@ -0,0 +1,112 @@
+const Note = require("../models/Note");
+const aysncHandler = require("express-async-handler");
+const User = require("../models/User");
+
+// @desc Get all notes
+// @route GET /notes
+// @access Private
+exports.getAllNotes = aysncHandler(async (req, res) => {
+  const notes = await Note.find().lean();
+  if (!notes?.length) {
+    return res.status(400).json({ message: "No notes found" });
+  }
+
+  // Add username to each note before sending response
+  const notesWithUser = await Promise.all(
+    notes?.map(async (note) => {
+      const user = await User.findById(note.user).exec();
+      return { ...note, username: user.username };
+    })
+  );
+  res.json(notesWithUser);
+});
+
+// @desc Create New Note
+// @route POST /notes
+// @access Private
+exports.createNewNote = aysncHandler(async (req, res) => {
+  const { user, title, text } = req.body;
+
+  // Confirm data
+  if (!user || !title || !text) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  // Check for duplicate title
+  const duplicate = await Note.findOne({ title }).lean().exec();
+
+  if (duplicate) {
+    return res.status(409).json({ message: "Duplicate note title" });
+  }
+
+  // Create and store the new user
+  const note = await Note.create({ user, title, text });
+
+  if (note) {
+    // Created
+    return res.status(201).json({ message: "New note created" });
+  } else {
+    return res.status(400).json({ message: "Invalid note data received" });
+  }
+});
+
+// @desc Update Note
+// @route PATCH /notes
+// @access Private
+exports.updateNote = aysncHandler(async (req, res) => {
+  const { id, user, title, text, completed } = req.body;
+
+  // Confirm data
+  if (!id || !user || !title || !text || typeof completed !== "boolean") {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  // Confirm note exists to update
+  const note = await Note.findById(id).exec();
+
+  if (!note) {
+    return res.status(400).json({ message: "Note not found" });
+  }
+
+  // Check for duplicate title
+  const duplicate = await Note.findOne({ title }).lean().exec();
+
+  // Allow renaming of the original note
+  if (duplicate && duplicate?._id.toString() !== id) {
+    return res.status(409).json({ message: "Duplicate note title" });
+  }
+
+  note.user = user;
+  note.title = title;
+  note.text = text;
+  note.completed = completed;
+
+  const updatedNote = await note.save();
+
+  res.json(`'${updatedNote.title}' updated`);
+});
+
+// @desc Delete Note
+// @route DELETE /notes
+// @access Private
+exports.deleteNote = aysncHandler(async (req, res) => {
+  const { id } = req.body;
+
+  // Confirm data
+  if (!id) {
+    return res.status(400).json({ message: "Note ID required" });
+  }
+
+  // Confirm note exists to delete
+  const note = await Note.findById(id).exec();
+
+  if (!note) {
+    return res.status(400).json({ message: "Note not found" });
+  }
+
+  const result = await note.deleteOne();
+
+  const reply = `Note '${result.title}' with ID ${result._id} deleted`;
+
+  res.json(reply);
+});
